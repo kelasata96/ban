2df346cc96613d08ca9eb1f3e105df7aa5e14732
kelasata96 <149935936+kelasata96@users.noreply.github.com>
2023-11-05 17:00:40 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'b6affc34b7ca34a5c1cdc843f15f37d7',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/b6affc34b7ca34a5c1cdc843f15f37d7/invoke.js"></scr' + 'ipt>');
